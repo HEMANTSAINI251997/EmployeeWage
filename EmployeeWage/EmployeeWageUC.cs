7ee2a0c173fe89bc8c7d587e953ed857a9d1a044
HEMANTSAINI251997 <hemantsaini2525@gmail.com>
2023-06-21 18:45:46 +0530

    [HEMANT]Add:Check Employee Present or Absent

---
+++ EmployeeWage/EmployeeWageUC.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class EmployeeWageUC
+    {
+        public void EmployeeWageFun()
+        {
+            int Is_Present = 1;
+            Random random = new Random();
+            int check = random.Next(0, 2);
+            if (check == Is_Present)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else 
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( "WELCOME TO EMPLOYEE WAGE COMPUTATION" );
+            EmployeeWageUC employeeWageUC = new EmployeeWageUC();
+            employeeWageUC.EmployeeWageFun();
+
+        }
+    }
+}
