fb8540fdad74e76c5eac8934f81e630607868614
HEMANTSAINI251997 <hemantsaini2525@gmail.com>
2023-06-22 15:39:51 +0530

    [HEMANT] ADD: Calculate Wages For A Month

--- EmployeeWage/EmployeeWageUC.cs
+++ EmployeeWage/EmployeeWageUC.cs
@@ -10,33 +10,60 @@ namespace EmployeeWage
     {
         public void EmployeeWageFun()
         {
-            int is_present = 1;
-            int is_parttime = 2;
+            const int is_present = 1;
+            const int is_parttime = 2;
             int half_day = 4;
             int full_day_hr = 8;
             int wage_per_hr = 20;
             int emp_hr = 0;
             int part_time = 8;
+            int totalemphr = 0;
             Random random = new Random();
-            int check = random.Next(0, 3);
-            
-            if (check == is_present)
+
+
+            for (int day = 0; day <= 20; day++)
             {
-                Console.WriteLine("Employee is Present");
-                emp_hr = full_day_hr;
+                int check = random.Next(0, 3);
+                switch (check)
+                {
+                    case is_present:
+                        emp_hr = full_day_hr;
+                        break;
+                    case is_parttime:
+                        emp_hr = half_day;
+                        break;
+                    default:
+                        emp_hr = 0;
+                        break;
+
+
+                }
+
+                //if (check == is_present)
+                //{
+                //   // Console.WriteLine("Employee is Present");
+                //    emp_hr = full_day_hr;
+                //}
+                //else if (check == is_parttime)
+                //{
+                //    //Console.WriteLine("Employee is on Part Time");
+                //    emp_hr = half_day;
+                //}
+                //else
+                //{
+                //    //Console.WriteLine("Employee is Absent");
+                //    emp_hr = 0;
+                //}
+
+                totalemphr += emp_hr;
+
+
             }
-            else if (check == is_parttime)
-            {
-                Console.WriteLine( "Employee is on Part Time" );
-                emp_hr = half_day;
-            }
-            else 
-            {
-                Console.WriteLine("Employee is Absent");
-                emp_hr = 0;
-            }
-            int totalwage = emp_hr * wage_per_hr;
-            Console.WriteLine("Total wage of Employee is = " + totalwage);
+            Console.WriteLine("Total Employee Hours for 20 Days =" + totalemphr);
+            int totalwage = totalemphr * wage_per_hr;
+            Console.WriteLine("Total wage of Employee 20 days is = " + totalwage);
+
         }
+
     }
 }
