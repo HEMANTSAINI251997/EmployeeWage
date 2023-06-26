d51f58545dce2f1f7afcfc3b492e8ffc5147f3ba
HEMANTSAINI251997 <hemantsaini2525@gmail.com>
2023-06-26 20:50:32 +0530

    [HEMANT]Add: Employee is Present or Absent

--- EmployeeWage/EmployeeWageUC.cs
+++ EmployeeWage/EmployeeWageUC.cs
@@ -10,87 +10,17 @@ namespace EmployeeWage
     {
         public void EmployeeWageFun()
         {
-            const int is_present = 1;
-            const int is_parttime = 2;
-            int half_day = 4;
-            int full_day_hr = 8;
-            int wage_per_hr = 20;
-            int emp_hr = 0;
-            int part_time = 8;
-            int totalemphr = 0;
-            int totaldays = 0;
-            int totalwage = 0;
+            int Is_Present = 1;
             Random random = new Random();
-
-
-            //for (int day = 0; day <= 20; day++)
-            //{
-            //    int check = random.Next(0, 3);
-            //    switch (check)
-            //    {
-            //        case is_present:
-            //            emp_hr = full_day_hr;
-            //            break;
-            //        case is_parttime:
-            //            emp_hr = half_day;
-            //            break;
-            //        default:
-            //            emp_hr = 0;
-            //            break;
-
-
-            //    }
-
-            //if (check == is_present)
-            //{
-            //   // Console.WriteLine("Employee is Present");
-            //    emp_hr = full_day_hr;
-            //}
-            //else if (check == is_parttime)
-            //{
-            //    //Console.WriteLine("Employee is on Part Time");
-            //    emp_hr = half_day;
-            //}
-            //else
-            //{
-            //    //Console.WriteLine("Employee is Absent");
-            //    emp_hr = 0;
-            //}
-
-            //        totalemphr += emp_hr;
-
-
-            //    }
-            //    Console.WriteLine("Total Employee Hours for 20 Days =" + totalemphr);
-            //    int totalwage = totalemphr * wage_per_hr;
-            //    Console.WriteLine("Total wage of Employee 20 days is = " + totalwage);
-
-            while (totalemphr < 100 && totaldays < 20)
+            int check = random.Next(0, 2);
+            if (check == Is_Present)
             {
-                int check = random.Next(0, 3);
-                switch (check)
-                {
-                    case is_present:
-                        emp_hr = full_day_hr;
-                        break;
-                    case is_parttime:
-                        emp_hr = half_day;
-                        break;
-                    default:
-                        emp_hr = 0;
-                        break;
-                }
-
-                totalemphr += emp_hr;
-                totaldays++;
-                int dailyWage = emp_hr * wage_per_hr;
-                totalwage += dailyWage;
+                Console.WriteLine("Employee is Present");
             }
-
-            Console.WriteLine("Total Employee Hours for 20 Days = " + totalemphr);
-            Console.WriteLine("Total Wage of Employee for 20 Days = " + totalwage);
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
-
-    
 }
