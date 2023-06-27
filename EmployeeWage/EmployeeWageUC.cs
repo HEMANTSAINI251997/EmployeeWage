e452255a7ac99e6aa3a2c639941f74a58c43d13e
HEMANTSAINI251997 <hemantsaini2525@gmail.com>
2023-06-27 14:47:46 +0530

    [HEMANT]Add: Calculate Wage for a Month

--- EmployeeWage/EmployeeWageUC.cs
+++ EmployeeWage/EmployeeWageUC.cs
@@ -8,41 +8,42 @@ namespace EmployeeWage
 {
     public class EmployeeWageUC
     {
+        const int is_present = 1;
+        const int is_parttime = 2;
         public void EmployeeWageFun()
         {
-            const int is_present = 1;
-            const int is_parttime = 2;
+
             int half_day = 4;
             int full_day_hr = 8;
             int wage_per_hr = 20;
             int emp_hr = 0;
-            int part_time = 8;
+            //int part_time = 8;
             int totalemphr = 0;
             Random random = new Random();
-            int check = random.Next(0, 3);
-            switch (check)
+            for (int day = 0; day <= 20; day++)
             {
-                case is_present:
-                    Console.WriteLine("Employee is Present");
-                    emp_hr = full_day_hr;
-                    break;
-                case is_parttime:
-                    Console.WriteLine("Employee is on Part Time");
-                    emp_hr = half_day;
-                    break;
-                default:
-                    Console.WriteLine("Employee is Absent");
-                    emp_hr = 0;
-                    break;
+                int check = random.Next(0, 3);
+                switch (check)
+                {
+                    case is_present:
+                        emp_hr = full_day_hr;
+                        break;
+                    case is_parttime:
+                        emp_hr = half_day;
+                        break;
+                    default:
+                        emp_hr = 0;
+                        break;
 
 
+                }
+                totalemphr += emp_hr;              
             }
-            int totalwage = emp_hr * wage_per_hr;
-            Console.WriteLine("Total wage of Employee is = " + totalwage);
-
-
-
+            Console.WriteLine("Total Employee Hours for 20 Days =" + totalemphr);
+            int totalwage = totalemphr * wage_per_hr;
+            Console.WriteLine("Total wage of Employee 20 days is = " + totalwage);
         }
 
     }
 }
+
