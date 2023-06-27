56db3eedf6678f8eb0efa05b27943b785459a6ed
HEMANTSAINI251997 <hemantsaini2525@gmail.com>
2023-06-27 11:26:25 +0530

    [HEMANT]Add: Calculate Daily Employee Wage

--- EmployeeWage/EmployeeWageUC.cs
+++ EmployeeWage/EmployeeWageUC.cs
@@ -10,17 +10,25 @@ namespace EmployeeWage
     {
         public void EmployeeWageFun()
         {
-            int Is_Present = 1;
+            int is_present = 1;
+            int full_day_hr = 8;
+            int wage_per_hr = 20;
+            int emp_hr = 0;
             Random random = new Random();
             int check = random.Next(0, 2);
-            if (check == Is_Present)
+
+            if (check == is_present)
             {
                 Console.WriteLine("Employee is Present");
+                emp_hr = full_day_hr;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
+                emp_hr = 0;
             }
+            int totalwage = emp_hr * wage_per_hr;
+            Console.WriteLine("Total wage of Employee is = " + totalwage);
         }
     }
 }
