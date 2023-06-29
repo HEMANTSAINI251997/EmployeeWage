b036ec4802d13ccc0d7bccdd9b21061fa0ce13fa
HEMANTSAINI251997 <hemantsaini2525@gmail.com>
2023-06-29 16:39:34 +0530

    [HEMANT]Add: Calc wages for Multiple Companies

--- EmployeeWage/EmployeeWageUC.cs
+++ EmployeeWage/EmployeeWageUC.cs
@@ -10,20 +10,25 @@ namespace EmployeeWage
     {
         const int is_present = 1;
         const int is_parttime = 2;
-        public static void EmployeeWageFun()
-        { 
+        public static void EmployeeWageFun(string company, int wage_per_hr, int workingdays, int workinghours)
+        {
             int half_day = 4;
             int full_day_hr = 8;
-            int wage_per_hr = 20;
-            int emp_hr = 0;
             int totalemphr = 0;
             int totaldays = 0;
-            int totalwage = 0;
+            int total_month_wage = 0;
+            int emp_hr = 0;
+            int totalworkinghr = 0;
             Random random = new Random();
+            //Console.WriteLine("enter the emp hours");
+            //int totalhrs = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("enter the total days");
+            //int days = Convert.ToInt32(Console.ReadLine());
 
-            while (totalemphr <= 100 && totaldays <= 20)
+            while (totalemphr <= workinghours && totaldays <= workingdays)
             {
                 int check = random.Next(0, 3);
+                totaldays++;
                 switch (check)
                 {
                     case is_present:
@@ -36,15 +41,14 @@ namespace EmployeeWage
                         emp_hr = 0;
                         break;
                 }
-
+                totalworkinghr += emp_hr;
                 totalemphr += emp_hr;
-                totaldays++;
-                int dailyWage = emp_hr * wage_per_hr;
-                totalwage += dailyWage;
-            }
 
-            Console.WriteLine("Total Employee Hours for 20 Days = " + totalemphr);
-            Console.WriteLine("Total Wage of Employee for 20 Days = " + totalwage);
+
+            }
+            total_month_wage = totalworkinghr * wage_per_hr;
+
+            Console.WriteLine("Total Wage of Employee at " + company + " for the month = " + total_month_wage);
         }
     }
 }
--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -11,10 +11,50 @@ namespace EmployeeWage
         static void Main(string[] args)
         {
             Console.WriteLine( "WELCOME TO EMPLOYEE WAGE COMPUTATION" );
-            //EmployeeWageUC employeeWageUC = new EmployeeWageUC();
-            //employeeWageUC.EmployeeWageFun();
-            EmployeeWageUC.EmployeeWageFun();
+            
+            // Calculate employee wages for Company A
+            string companyA = "Company A";
+            int wageperhourA = 10;
+            int workingdaysA = 20;
+            int workinghoursA = 100;
+            EmployeeWageUC.EmployeeWageFun(companyA, wageperhourA, workingdaysA, workinghoursA);
+
+            // Calculate employee wages for Company B
+            string companyB = "Company B";
+            int wageperhourB = 12;
+            int workingdaysB = 22;
+            int workinghoursB = 110;
+            EmployeeWageUC.EmployeeWageFun(companyB, wageperhourB, workingdaysB, workinghoursB);
+
+            // Calculate employee wages for Company C
+            string companyC = "Company C";
+            int wageperhourC = 8;
+            int workingdaysC = 18;
+            int workinghoursC = 90;
+            EmployeeWageUC.EmployeeWageFun(companyC, wageperhourC, workingdaysC, workinghoursC);
 
         }
     }
 }
+
+//EmployeeWageUC employeeWageUC = new EmployeeWageUC();
+//employeeWageUC.EmployeeWageFun();
+//Console.WriteLine("How many companies data you want to enter  :");
+//int num = Convert.ToInt32(Console.ReadLine());
+//string name = "";
+//int emphr = 0;
+//int wage = 0;
+//int hrmonth = 0;
+//for (int i = 1; i <= num; i++)
+//{
+//    Console.WriteLine("Enter the company names");
+//    name = Console.ReadLine();
+//    Console.WriteLine("Enter the number of working of Employee in hours");
+//    emphr = Convert.ToInt32(Console.ReadLine());
+//    Console.WriteLine("Enter the number of Employee wage");
+//    wage = Convert.ToInt32(Console.ReadLine());
+//    Console.WriteLine("Enter the number of working hours per month");
+//    hrmonth = Convert.ToInt32(Console.ReadLine());
+
+
+//}
